2287b94b8fe46054ee336fd88a9dc579eedbf0e7
Maxhy <maxime.chamley@leosac.com>
2023-03-02 11:27:17 +0100

    Use webpack for build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,39 @@
+const path = require('path');
+
 module.exports = {
-  entry: './cardeditor.js',
+  entry: './src/lib/index.js',
   output: {
     filename: 'cardeditor.js',
     library: 'cardeditor',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: {node: "current"} }],
+              "@babel/preset-react"
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
       {
         test: /src\/locales\/*\/.json/,
         loader: "@alienfast/i18next-loader",
       }
     ],
+  },
+  externals: {
+    'react': 'react',
+    'react-dom': 'react-dom',
   }
 };
